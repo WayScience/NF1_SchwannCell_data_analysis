db8081a113cc5bec9db1249d77706d3fd450a6c5
Cameron Mattson <camo@oak.ucdenver.pvt>
2023-05-19 09:53:47 -0600

    Changed the data path and added review changes

--- 0.data_analysis/nbconverted/plates_1_2_sig_test_nf1_sc_all_cellprofiler.py
+++ 0.data_analysis/nbconverted/plates_1_2_sig_test_nf1_sc_all_cellprofiler.py
@@ -3,18 +3,22 @@
 
 # # Obnibus and post hoc testing with Anova and scheffe's test
 
+# Obnibius Testing: https://www.statology.org/omnibus-test/
+#
+# Post hoc Testins: https://www.statology.org/anova-post-hoc-tests/
+
 # ## Imports
 
 # In[1]:
 
 
 import sys
-from sklearn.ensemble import IsolationForest
-import scikit_posthocs as sp
 from pathlib import Path
+
 import numpy as np
 import pandas as pd
-from collections import defaultdict
+import scikit_posthocs as sp
+from sklearn.ensemble import IsolationForest
 
 sys.path.append("../utils")
 import analysis_utils as au
@@ -24,48 +28,52 @@ import preprocess_utils as ppu
 # In[2]:
 
 
-rnd_val = 0 # Random value for all seeds
-rng = np.random.default_rng(seed=rnd_val) # random number generator
+rnd_val = 0  # Random value for all seeds
+rng = np.random.default_rng(seed=rnd_val)  # random number generator
 
 
 # # Preprocess data using preprocess utils
 
+# ## Remove Outliers
+
 # In[3]:
 
 
-current_dir = Path.cwd()
-print(current_dir)
+plates = {}
+
+plates["1"] = {
+    "path": "../nf1_painting_repo/3.processing_features/data/feature_selected_data/Plate_1_sc_norm_fs.parquet"
+}
+plates["2"] = {
+    "path": "../nf1_painting_repo/3.processing_features/data/feature_selected_data/Plate_2_sc_norm_fs.parquet"
+}
+
+for plate, vals in plates.items():
+    # Initializes the preprocessing class
+    po = ppu.Preprocess_data(path=vals["path"])
+
+    # Gets the dataframe after removing metadata columns,
+    # except for the 'Metadata_genotype' column specified above
+    platedf = po.remove_meta(po.df, kept_meta_columns=["Metadata_genotype"])
+
+    # Use isolation forest to select inliers:
+    isof = IsolationForest(random_state=rnd_val)
+    out_preds = isof.fit_predict(platedf.drop(columns=["Metadata_genotype"]))
+    ind = np.nonzero(out_preds == 1)[0]  # Select inliers
+
+    # Select inlier samples:
+    plates[plate]["df"] = platedf.iloc[ind]
+
+platesdf = [plates[key]["df"] for key in plates]
 
 
 # In[4]:
 
 
-filename = 'nf1_sc_all_cellprofiler.csv.gz'
-base_path = Path("../nf1_benchmark_repo/4_processing_features/data")
-ending_path = Path("CellProfiler/nf1_sc_all_cellprofiler.csv.gz")
-
-plates = {}
-
-plates['1'], plates['2'] = {"path": base_path / "Plate1" / ending_path}, {"path": base_path / "Plate2" / ending_path}
-
-for plate, vals in plates.items():
-    # Initializes the preprocessing class
-    po = ppu.Preprocess_data(path=vals['path'])
-    
-    # Gets the dataframe after removing metadata columns, 
-    # except for the 'Metadata_genotype' column specified above
-    platedf = po.remove_meta(po.df, kept_meta_columns=['Metadata_genotype'])
-    platedf.drop(['Cytoplasm_Number_Object_Number'], axis=1, inplace=True)
-    
-    # Use isolation forest to select inliers:
-    isof = IsolationForest(random_state = rnd_val) 
-    out_preds = isof.fit_predict(platedf.drop(columns=['Metadata_genotype']))
-    ind = np.nonzero(out_preds == 1)[0] # Select inliers
-    
-    # Select inlier samples:
-    plates[plate]['df'] = platedf.iloc[ind]
-    
-platesdf = [plates[key]['df'] for key in plates]
+### Use only the common columns between both plates:
+common_columns = list(platesdf[0].columns.intersection(platesdf[1].columns))
+platesdf[0] = platesdf[0].loc[:, common_columns]
+platesdf[1] = platesdf[1].loc[:, common_columns]
 
 
 # # Conduct testing with scheffe's test
@@ -73,11 +81,9 @@ platesdf = [plates[key]['df'] for key in plates]
 # In[5]:
 
 
-#gtypes = ['Null','WT'] # The 2 types of genotypes
 test = sp.posthoc_scheffe
 # Pass the plates dataframes in order, so that genotypes are suffixed corretly (eg. WT1WT2)
 # Plates are 1 indexed
-# res_test = au.sig_test(test, platesdf)
 st = au.Sig_testing(platesdf)
 anova_feats, sig_anova_pvals = st.anova_test()
 res_test = st.posthoc_test(anova_feats, sig_anova_pvals, test)
@@ -88,26 +94,29 @@ res_test = st.posthoc_test(anova_feats, sig_anova_pvals, test)
 
 # Returns a dictionary of genotype pairs containing the significant column names
 # being analyzed
-sig_groups = st.get_columns(res_test['sig_feat_phoc'])
+sig_groups = st.get_columns(res_test["sig_feat_phoc"])
 
 
 # In[7]:
 
 
 tot_columns = len(platesdf[0].columns)
-sig_groupsdf = pd.DataFrame(list({grp: len(tot) for grp, tot in sig_groups.items()}.items()),
-                            columns=['genotype', 'number_of_significant_columns'])
-sig_groupsdf['proportion_of_significant_columns'] = sig_groupsdf['number_of_significant_columns'] / tot_columns
-sig_groupsdf = sig_groupsdf.round({'proportion_of_significant_columns': 2})
+sig_groupsdf = pd.DataFrame(
+    list({grp: len(tot) for grp, tot in sig_groups.items()}.items()),
+    columns=["genotype", "number_of_significant_columns"],
+)
+sig_groupsdf["proportion_of_significant_columns"] = (
+    sig_groupsdf["number_of_significant_columns"] / tot_columns
+)
+sig_groupsdf = sig_groupsdf.round({"proportion_of_significant_columns": 2})
 
 
 # In[8]:
 
 
-out_file = Path('data/plate_1_2_genotype_signifance.tsv')
+out_file = Path("data/plate_1_2_genotype_signifance.tsv")
 
 if not out_file.parent.is_dir():
     out_file.parent.mkdir(parents=True, exist_ok=True)
 
-sig_groupsdf.to_csv(out_file, sep='\t', index=False)
-
+sig_groupsdf.to_csv(out_file, sep="\t", index=False)
--- 0.data_analysis/nbconverted/plates_1_2_visualization_nf1_sc_all_cellprofiler.py
+++ 0.data_analysis/nbconverted/plates_1_2_visualization_nf1_sc_all_cellprofiler.py
@@ -8,10 +8,11 @@
 
 import sys
 from pathlib import Path
+
 import pandas as pd
 from sklearn.preprocessing import LabelBinarizer as labi
 
-sys.path.append(f'../utils')
+sys.path.append(f"../utils")
 import analysis_utils as au
 import preprocess_utils as ppu
 
@@ -21,12 +22,14 @@ import preprocess_utils as ppu
 # In[2]:
 
 
-filename = 'nf1_sc_all_cellprofiler.csv.gz'
-base_path = Path("../nf1_benchmark_repo/4_processing_features/data")
-ending_path = Path("CellProfiler/nf1_sc_all_cellprofiler.csv.gz")
 plates = {}
 
-plates['1'], plates['2'] = {"path": base_path / "Plate1" / ending_path}, {"path": base_path / "Plate2" / ending_path}
+plates["1"] = {
+    "path": "../nf1_painting_repo/3.processing_features/data/feature_selected_data/Plate_1_sc_norm_fs.parquet"
+}
+plates["2"] = {
+    "path": "../nf1_painting_repo/3.processing_features/data/feature_selected_data/Plate_2_sc_norm_fs.parquet"
+}
 
 
 # In[3]:
@@ -34,16 +37,16 @@ plates['1'], plates['2'] = {"path": base_path / "Plate1" / ending_path}, {"path"
 
 for plate, vals in plates.items():
     # Initializes the preprocessing class
-    po = ppu.Preprocess_data(path=vals['path'])
-    
-    # Gets the dataframe after removing metadata columns, 
+    po = ppu.Preprocess_data(path=vals["path"])
+
+    # Gets the dataframe after removing metadata columns,
     # except for the 'Metadata_genotype' column specified above
-    platedf = po.remove_meta(po.df, kept_meta_columns=['Metadata_genotype'])
-    
-    platedf['labels'] = platedf['Metadata_genotype'] + plate
-    platedf.drop(['Cytoplasm_Number_Object_Number','Metadata_genotype'], axis=1, inplace=True)
-    
-    plates[plate]['df'] = platedf
+    platedf = po.remove_meta(po.df, kept_meta_columns=["Metadata_genotype"])
+
+    platedf["labels"] = platedf["Metadata_genotype"] + plate
+    platedf.drop(["Metadata_genotype"], axis=1, inplace=True)
+
+    plates[plate]["df"] = platedf
 
 
 # # Combining Data
@@ -51,103 +54,170 @@ for plate, vals in plates.items():
 # In[4]:
 
 
-plate1df = plates['1']['df']
-plate2df = plates['2']['df']
+plate1df = plates["1"]["df"]
+plate2df = plates["2"]["df"]
 
 
 # In[5]:
 
 
-platescomb = pd.concat([plate1df, plate2df], axis=0)
-
-plateswt = platescomb.loc[~platescomb['labels'].str.contains('Null')]
-platesnull = platescomb.loc[~platescomb['labels'].str.contains('WT')]
+### Use only the common columns between both plates:
+common_columns = list(plate1df.columns.intersection(plate2df.columns))
+plate1df = plate1df.loc[:, common_columns]
+plate2df = plate2df.loc[:, common_columns]
 
 
 # In[6]:
 
 
-plateswt = platescomb[~platescomb.iloc[:,-1].str.contains('Null')]
-platesnull = platescomb[~platescomb.iloc[:,-1].str.contains('WT')]
+platescomb = pd.concat([plate1df, plate2df], axis=0)
 
+plateswt = platescomb.loc[~platescomb["labels"].str.contains("Null")]
+platesnull = platescomb.loc[~platescomb["labels"].str.contains("WT")]
 
-# # Visualization
 
 # In[7]:
 
 
-out_path = Path('figures')
-
-if not out_path.exists():
-    out_path.mkdir(parents=True, exist_ok=True)
+platescomb
 
 
 # In[8]:
 
 
-save_args = {'fname': out_path / 'pca_plate1'}
-au.plot_pca(feats=plate1df.drop('labels', axis=1), labels=plate1df['labels'], save_args=save_args, title='PCA of Plate 1')
+plateswt = platescomb[~platescomb.iloc[:, -1].str.contains("Null")]
+platesnull = platescomb[~platescomb.iloc[:, -1].str.contains("WT")]
 
+
+# # Visualization
 
 # In[9]:
 
 
-save_args = {'fname': out_path / 'umap_plate1'}
-au.plot_umap(feats=plate1df.drop('labels', axis=1), labels=plate1df['labels'], save_args=save_args, title='UMAP of Plate 1')
+out_path = Path("figures")
+
+if not out_path.exists():
+    out_path.mkdir(parents=True, exist_ok=True)
 
 
 # In[10]:
 
 
-save_args = {'fname': out_path / 'pca_plate2'}
-au.plot_pca(feats=plate2df.drop('labels', axis=1), labels=plate2df['labels'], save_args=save_args, title='PCA of Plate 2')
+save_args = {"fname": out_path / "pca_plate1"}
+au.plot_pca(
+    feats=plate1df.drop("labels", axis=1),
+    labels=plate1df["labels"],
+    save_args=save_args,
+    title="PCA of Plate 1",
+)
 
 
 # In[11]:
 
 
-save_args = {'fname': out_path / 'umap_plate2'}
-au.plot_umap(feats=plate2df.drop('labels', axis=1), labels=plate2df['labels'], save_args=save_args, loc='upper left', title='UMAP of Plate 2')
+save_args = {"fname": out_path / "umap_plate1"}
+au.plot_umap(
+    feats=plate1df.drop("labels", axis=1),
+    labels=plate1df["labels"],
+    save_args=save_args,
+    title="UMAP of Plate 1",
+)
 
 
 # In[12]:
 
 
-save_args = {'fname': out_path / 'pca_plates_1_2'}
-au.plot_pca(feats=platescomb.drop('labels', axis=1), labels=platescomb['labels'], save_args=save_args, title='PCA of Plates 1 and 2')
+save_args = {"fname": out_path / "pca_plate2"}
+au.plot_pca(
+    feats=plate2df.drop("labels", axis=1),
+    labels=plate2df["labels"],
+    save_args=save_args,
+    title="PCA of Plate 2",
+)
 
 
 # In[13]:
 
 
-save_args = {'fname': out_path / 'umap_plates_1_2'}
-au.plot_umap(feats=platescomb.drop('labels', axis=1), labels=platescomb['labels'], save_args=save_args, loc='upper right', title='UMAP of Plates 1 and 2')
+save_args = {"fname": out_path / "umap_plate2"}
+au.plot_umap(
+    feats=plate2df.drop("labels", axis=1),
+    labels=plate2df["labels"],
+    save_args=save_args,
+    loc="upper left",
+    title="UMAP of Plate 2",
+)
 
 
 # In[14]:
 
 
-save_args = {'fname': out_path / 'umap_wt_plates_1_2'}
-au.plot_umap(feats=plateswt.drop('labels', axis=1), labels=plateswt['labels'], save_args=save_args, loc='lower left', title='UMAP of Plates 1 and 2')
+save_args = {"fname": out_path / "pca_plates_1_2"}
+au.plot_pca(
+    feats=platescomb.drop("labels", axis=1),
+    labels=platescomb["labels"],
+    save_args=save_args,
+    title="PCA of Plates 1 and 2",
+)
 
 
 # In[15]:
 
 
-save_args = {'fname': out_path / 'pca_wt_plates_1_2'}
-au.plot_pca(feats=plateswt.drop('labels', axis=1), labels=plateswt['labels'], save_args=save_args, title='PCA of Plates 1 and 2')
+save_args = {"fname": out_path / "umap_plates_1_2"}
+au.plot_umap(
+    feats=platescomb.drop("labels", axis=1),
+    labels=platescomb["labels"],
+    save_args=save_args,
+    loc="upper right",
+    title="UMAP of Plates 1 and 2",
+)
 
 
 # In[16]:
 
 
-save_args = {'fname': out_path / 'umap_null_plates_1_2'}
-au.plot_umap(feats=platesnull.drop('labels', axis=1), labels=platesnull['labels'], save_args=save_args, title='UMAP of Plates 1 and 2')
+save_args = {"fname": out_path / "umap_wt_plates_1_2"}
+au.plot_umap(
+    feats=plateswt.drop("labels", axis=1),
+    labels=plateswt["labels"],
+    save_args=save_args,
+    loc="lower left",
+    title="UMAP of Plates 1 and 2",
+)
 
 
 # In[17]:
 
 
-save_args = {'fname': out_path / 'pca_null_plates_1_2'}
-au.plot_pca(feats=platesnull.drop('labels', axis=1), labels=platesnull['labels'], save_args=save_args, title='PCA of Plates 1 and 2')
+save_args = {"fname": out_path / "pca_wt_plates_1_2"}
+au.plot_pca(
+    feats=plateswt.drop("labels", axis=1),
+    labels=plateswt["labels"],
+    save_args=save_args,
+    title="PCA of Plates 1 and 2",
+)
 
+
+# In[18]:
+
+
+save_args = {"fname": out_path / "umap_null_plates_1_2"}
+au.plot_umap(
+    feats=platesnull.drop("labels", axis=1),
+    labels=platesnull["labels"],
+    save_args=save_args,
+    title="UMAP of Plates 1 and 2",
+)
+
+
+# In[19]:
+
+
+save_args = {"fname": out_path / "pca_null_plates_1_2"}
+au.plot_pca(
+    feats=platesnull.drop("labels", axis=1),
+    labels=platesnull["labels"],
+    save_args=save_args,
+    title="PCA of Plates 1 and 2",
+)
