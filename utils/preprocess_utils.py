000c609526a83f9c18941fc3b38e4c6649afe725
Cameron Mattson <camo@oak.ucdenver.pvt>
2023-06-09 09:34:42 -0600

    Refactored to improve usability

--- utils/preprocess_utils.py
+++ utils/preprocess_utils.py
@@ -2,15 +2,15 @@ import pandas as pd
 import numpy as np
 import pathlib
 from sklearn.preprocessing import LabelBinarizer
-import os
+
 
 class Preprocess_data:
     """
     A simplified means to retrieving dataframes from files and removing choosen metadata.
     """
 
-    rnd_val = 0 # Random value for all seeds
-    rng = np.random.default_rng(seed=rnd_val) # random number generator
+    rnd_val = 0  # Random value for all seeds
+    rng = np.random.default_rng(seed=rnd_val)  # random number generator
 
     def __init__(self, path):
         """
@@ -22,34 +22,38 @@ class Preprocess_data:
             Metadata column names to be kept in the retrieved dataframe (optional)
         """
         path = pathlib.Path(path)
-                     
+
         # If the file isn't found in the path above then raise an error.
         if not path.is_file():
             raise FileNotFoundError(f"File '{path}' does not exist")
-            
-        if 'parquet' in path.name:
-            self.df = pd.read_parquet(path)
-            
-        elif 'csv' in path.name:
-            self.df = pd.read_csv(path)
-            
-        elif 'tsv' in path.name:
-            self.df = pd.read_csv(path, delimiter='\t')
-            
-        else:
-            raise ValueError("The file must be a parquet, csv, or tsv, with the applicable extension included in the filename.")
 
-        self.df = self.df.loc[:,self.df.columns != 'Unnamed: 0'] # Remove the unnamed column
+        if "parquet" in path.name:
+            self.df = pd.read_parquet(path)
+
+        elif "csv" in path.name:
+            self.df = pd.read_csv(path)
+
+        elif "tsv" in path.name:
+            self.df = pd.read_csv(path, delimiter="\t")
+
+        else:
+            raise ValueError(
+                "The file must be a parquet, csv, or tsv, with the applicable extension included in the filename."
+            )
+
+        self.df = self.df.loc[
+            :, self.df.columns != "Unnamed: 0"
+        ]  # Remove the unnamed column
 
     def remove_meta(self, df, kept_meta_columns=None):
         """
         This function removes all metadata columns, except the ones specified.
-        
+
         Parameters
         ----------
         df: Pandas Dataframe
             The dataframe to be sampled.
-            
+
         kept_meta_columns: List of strings
             The columns in the dataframe that should be retained (optional)
 
@@ -58,10 +62,13 @@ class Preprocess_data:
         Pandas Dataframe
             The dataframe without all metadata columns, excluding the specified metadata columns.
         """
-        feat_col = [col for col in df.columns if 'Metadata' not in col] # Select all columns that don't contain the Metadata in their name
+        feat_col = [
+            col for col in df.columns if "Metadata" not in col
+        ]  # Select all columns that don't contain the Metadata in their name
         if kept_meta_columns is not None:
             kept_col_df = df[kept_meta_columns]
             return pd.concat([kept_col_df, df[feat_col]], axis=1)
         else:
             return df[feat_col]
+
         return df
